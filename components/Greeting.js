246b4d3623f722654bdf161d80ea839337beb781
mcvzone <mcvzone@naver.com>
2022-08-20 04:02:53 +0900

    add greeting component

---
+++ components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Greeting() {
+  return (
+    <View>
+      <Text>This is Greeting component.</Text>
+    </View>
+  );
+}
+
+export default Greeting;
